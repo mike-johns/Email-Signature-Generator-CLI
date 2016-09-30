63d433618e2ea6c0f396084dd1f3f1e9e2b14252
Mike Johns <mikejohns7@icloud.com>
2016-09-29 21:02:36 -0700

    some testing changes to app.rb

--- app.rb
+++ app.rb
@@ -16,69 +16,69 @@ require_relative 'colorizr'
 
 # Begin command line user interface walks the user through data entry
 
-puts ""
-puts "Welcome!".red
-puts "Welcome!".yellow
-puts "Welcome!".green
-puts "Welcome!".light_blue
-puts "Welcome!".blue
-puts ""
-puts "Let's get you all set up with a new signature. Press RETURN to start."
-x = gets
+# puts ""
+# puts "Welcome!".red
+# puts "Welcome!".yellow
+# puts "Welcome!".green
+# puts "Welcome!".light_blue
+# puts "Welcome!".blue
+# puts ""
+# puts "Let's get you all set up with a new signature. Press RETURN to start."
+# x = gets
 
-file = Upload.new
+# file = Upload.new
 
 # result = Signature.new first: "Mike", last: "Johns", title: "Admin", image_link: file.download_url
 
-#
-# puts ""
-# puts "Hey there!"
-# puts "I'll walk you through creating your email signature one step at a time."
-# puts ""
-#
-# puts "What's your first name?"
-# first_name = gets.chomp!
-#
-# puts "Sounds good, #{first_name}. Your last name?"
-# last_name = gets.chomp!
-#
-# puts "Great! What's your title at Thrive School?"
-# title_entry = gets.chomp!
-#
-# puts "What's your phone number?"
-# puts ">>> Write it out like this: 916-869-5309"
-# phone_entry = gets.chomp!
-#
-# puts "Do you have a Twitter account?"
-# puts ">>> Enter yes or no"
-# twitter_bool = gets.chomp!
-# if twitter_bool.upcase != "NO"
-#   puts "What's your username?"
-#   puts ">>> Don't worry about the @"
-#   twitter_entry = gets.chomp!
-# end
-#
-# puts "Alright, #{first_name}, we're almost done!"
-# puts ""
-#
-# puts "Now let's grab a profile picture."
-# puts "Do you have a link ready?"
-# puts ">>> Enter yes or no"
-# url_bool = gets.chomp!
-# if url_bool.upcase != "NO"
-#   puts "Paste it here."
-#   puts ">>> The whole thing, starting with http"
-#   url_entry = gets.chomp!
-# else
-#   puts "No worries. You can leave this window open until you do."
-#   puts "Paste it here when you're ready:"
-#   url_entry = gets.chomp!
-# end
-#
-# puts "Starting HTML creation process..."
-#
-# result = Signature.new first: first_name, last: last_name, title: title_entry, phone: phone_entry, twitter: twitter_entry, image_link: url_entry
-#
+
+puts ""
+puts "Hey there!"
+puts "I'll walk you through creating your email signature one step at a time."
+puts ""
+
+puts "What's your first name?"
+first_name = gets.chomp!
+
+puts "Sounds good, #{first_name}. Your last name?"
+last_name = gets.chomp!
+
+puts "Great! What's your title at Thrive School?"
+title_entry = gets.chomp!
+
+puts "What's your phone number?"
+puts ">>> Write it out like this: 916-869-5309"
+phone_entry = gets.chomp!
+
+puts "Do you have a Twitter account?"
+puts ">>> Enter yes or no"
+twitter_bool = gets.chomp!
+if twitter_bool.upcase != "NO"
+  puts "What's your username?"
+  puts ">>> Don't worry about the @"
+  twitter_entry = gets.chomp!
+end
+
+puts "Alright, #{first_name}, we're almost done!"
+puts ""
+
+puts "Now let's grab a profile picture."
+puts "Do you have a link ready?"
+puts ">>> Enter yes or no"
+url_bool = gets.chomp!
+if url_bool.upcase != "NO"
+  puts "Paste it here."
+  puts ">>> The whole thing, starting with http"
+  url_entry = gets.chomp!
+else
+  puts "No worries. You can leave this window open until you do."
+  puts "Paste it here when you're ready:"
+  url_entry = gets.chomp!
+end
+
+puts "Starting HTML creation process..."
+
+result = Signature.new first: first_name, last: last_name, title: title_entry, phone: phone_entry, twitter: twitter_entry, image_link: url_entry
+
 # puts "Done"
 # puts "There's a new file on your Desktop called #{result.file_name}"
 # puts "Here's how to add it to your Gmail:"
