57dcf460025c11c72e38c65ff4d5b5e0378b2690
Mike Johns <mikejohns7@icloud.com>
2016-10-10 20:20:10 -0700

    Update HTML template

--- templates.rb
+++ templates.rb
@@ -2,7 +2,6 @@ module Templates
   # TODO: Update with smaller sizing
   def generate_with_twitter
     html = <<-HEREDOC
-    <br>
     <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
       <tr>
         <td valign="top" style="padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0; border-right: solid 3px #333333">
@@ -14,7 +13,7 @@ module Templates
           <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
             <tr><td style="padding-bottom: 5px; color: #333333; font-size: 14px; font-family: Helvetica, sans-serif;"><strong>#{@first} #{@last}</strong></td></tr>
             <tr><td style="padding-bottom: 5px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif;">#{@title}, <strong>Thrive School</strong></td></tr>
-            <tr><td style="padding-bottom: 2px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif; font-weight: normal;"><a href="tel:+01-#{@phone}" style=" color: #F7751F; text-decoration: none;">cell</a> | <a href="mailto:#{@first.downcase}.#{@last.downcase}@thriveschool.email" style="color: #F7751F; text-decoration: none;">email</a> | <a href="https://twitter.com/#{@twitter}" style="color: #F7751F; text-decoration: none;">twitter</a> | <a href="http://thriveschool.info" style="color: #F7751F; text-decoration: none;">web</a></td></tr>
+            <tr><td style="padding-bottom: 2px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif; font-weight: normal;"><a href="tel:+01-#{@phone}" style="color: #F7751F; text-decoration: none;">cell</a> | <a href="mailto:#{@first.downcase}.#{@last.downcase}@thriveschool.email" style="color: #F7751F; text-decoration: none;">email</a> | <a href="https://twitter.com/#{@twitter}" style="color: #F7751F; text-decoration: none;">twitter</a> | <a href="https://app.hubspot.com/meetings/mike-johns" style="color: #F7751F; text-decoration: none;">meet</a></td></tr>
           </table>
         </td>
       </tr>
@@ -25,7 +24,6 @@ module Templates
 
   def generate_without_twitter
     html = <<-HEREDOC
-    <br>
     <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
       <tr>
         <td valign="top" style="padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0: border-left: 0; border-right: solid 3px #333333">
@@ -37,7 +35,7 @@ module Templates
           <table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
             <tr><td style="padding-bottom: 5px; color: #333333; font-size: 14px; font-family: Helvetica, sans-serif;"><strong>#{@first} #{@last}</strong></td></tr>
             <tr><td style="padding-bottom: 5px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif;">#{@title}, <strong>Thrive School</strong></td></tr>
-            <tr><td style="padding-bottom: 2px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif; font-weight: normal;"><a href="tel:+01-#{@phone}" style=" color: #F7751F; text-decoration: none;">cell</a> | <a href="mailto:#{@first.downcase}.#{@last.downcase}@thriveschool.email" style="color: #F7751F; text-decoration: none;">email</a> | <a href="http://thriveschool.info" style="color: #F7751F; text-decoration: none;">web</a></td></tr>
+            <tr><td style="padding-bottom: 2px; color: #333333; font-size: 12px; font-family: Helvetica, sans-serif; font-weight: normal;"><a href="tel:+01-#{@phone}" style="color: #F7751F; text-decoration: none;">cell</a> | <a href="mailto:#{@first.downcase}.#{@last.downcase}@thriveschool.email" style="color: #F7751F; text-decoration: none;">email</a> | <a href="https://app.hubspot.com/meetings/mike-johns" style="color: #F7751F; text-decoration: none;">meet</a></td></tr>
           </table>
         </td>
       </tr>
